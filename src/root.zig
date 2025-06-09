1ad7a022fa86a43161d4c0d68ae944be3d57b94e
Peter Mbanugo <p.mbanugo@yahoo.com>
2025-06-09 19:17:07 +0200

    chore

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,8 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
+//! Gotham HTTP Server library and micro-framework
+//! A high-performance HTTP server for Zig inspired by Node.js
+
+// By convention, root.zig is the root source file when making a library. If
+// you are making an executable, the convention is to delete this file and
+// start with main.zig instead.
 const std = @import("std");
 const testing = std.testing;
